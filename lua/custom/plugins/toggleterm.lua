7b6a391f0ce31809ca2eee0a5adba216e43ba4b0
John Hoffmann <john.hoffmann@kantar.com>
2025-06-16 18:38:57 -0600

    merge conflicts

---
+++ lua/custom/plugins/toggleterm.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    'akinsho/toggleterm.nvim',
+    version = '*',
+    config = true,
+    opts = {
+      size = 20,
+      float_opts = {
+        border = 'curved',
+        width = 120,
+        height = 40,
+        title_pos = 'left',
+      },
+    },
+  },
+
+  vim.keymap.set('n', '<A-i>', '<cmd>ToggleTerm direction=horizontal<cr>', { desc = 'Toggle a split terminal' }),
+  vim.keymap.set('n', '<A-f>', '<cmd>ToggleTerm direction=float<cr>', { desc = 'Toggle a floating terminal' }),
+}
